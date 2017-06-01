a6bffcf8edfbd2c0630256eba21bb7783abf76bb
Abdelaziz JRIBI <a.jribi.ext@paritel.fr>
2017-06-01 10:06:48 +0100

    fix bug adresse style

--- themes/classic/assets/js/custom.js
+++ themes/classic/assets/js/custom.js
@@ -2,3 +2,7 @@
  * Custom code goes here.
  * A template should always ship with an empty custom.js
  */
+$('.js-address-selector input[type=radio]').on('click',function(){
+    $('.address-item.selected').removeClass('selected');
+    $('.address-item:has(input[type=radio]:checked)').addClass('selected');
+});
